c656d775b5264416b9d5dc3808ce9489f1ad48e8
Graziela Ralph <113774678+grzlrlph@users.noreply.github.com>
2023-10-01 20:39:35 -0400

    :bug: fix: ligou cadastro e login a homepage
    nao tava ligado rs

---
+++ Redai2/app/src/main/java/com/example/redai/activities/Homepage.java
@@ -0,0 +1,16 @@
+package com.example.redai.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.redai.R;
+
+public class Homepage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+}
